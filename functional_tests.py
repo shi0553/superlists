41a8f1184df889ee8bf0412affc76b6859c02559
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-04-30 18:21:22 +0800

    DRY finish

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
        self. browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,  [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
@@ -34,11 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
-
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1: Buy peacock feathers' for row in rows),
-                        f"New to-do item did not appear in table. Contents were:\n{table.text}")
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
@@ -49,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(2)
         
         # 页面再次更新，她的清单中显示了这两个待办事项
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',  [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly' , [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 	
 		# 她很满意，然后去睡觉了		
         self.fail('Finish the test!')
