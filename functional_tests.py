166561a0eb605637b1b555910ee119eab07bc11b
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-04-30 16:07:08 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -20,10 +20,10 @@ class NewVisitorTest(unittest.TestCase):
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn( 'To-Do', self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
-        self.asserIn('To-Do', header_text)
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请她输入一个待办事项
-        inputbox=self.browser.find_element_by_id('id_new_itme')
+        inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # 她在一个文本框中输入了"Buy peacock feathers" (购买孔雀羽毛)
@@ -35,9 +35,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table=self.bowser.find_element_by_id('id_list_table')
+        table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1: Buy peacock feathers'))
+        self.assertTrue(any(row.text=='1: Buy peacock feathers' for row in rows),
+                        "New to-do item did not appear in table")
 		
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
