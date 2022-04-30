6f0eba8f6b5a56968ab4823a49e7150168cc45a4
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-04-30 16:36:59 +0800

    Dont repeat yourself

--- functional_tests.py
+++ functional_tests.py
@@ -33,18 +33,26 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
         self.assertTrue(any(row.text=='1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+                        f"New to-do item did not appear in table. Contents were:\n{table.text}")
 		
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
         # 伊迪丝做事很有条理
-	
-		# 页面再次更新，她的清单中显示了这两个待办事项
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+        
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',  [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly' , [row.text for row in rows])
 	
 		# 她很满意，然后去睡觉了		
         self.fail('Finish the test!')
