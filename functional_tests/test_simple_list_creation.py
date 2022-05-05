ae9a8ed5883badeec83b485e8bd7b2aef6ad2a09
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-05-05 20:58:35 +0800

    Moved FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,39 +1,8 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-import os
-from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT=10
-
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser=webdriver.Firefox()
-        print("start")
-        staging_server=os.environ.get('STAGING_SERVER')
-        print(staging_server)
-        if staging_server:
-            self.live_server_url='http://'+staging_server
-            print(self.live_server_url)
-
-    def tearDown(self):
-       self. browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time=time.time()
-        while True:
-            try:
-                table=self.browser.find_element_by_id('id_list_table')
-                rows=table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text,  [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time()-start_time>MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
@@ -69,24 +38,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 她很满意，然后去睡觉了		
-
-    def test_layout_and_styling(self):
-        #伊迪丝访问首页
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        #她看到输入框完美地居中显示
-        inputbox=self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,
-                               512, delta=10)
-
-        #她新建了一个清单,看到输入框仍完美居中显示
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox=self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,
-                               512, delta=10)
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # 伊迪斯新建一个待办事项清单
@@ -133,9 +84,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 两人都很满意，然后去睡觉了
 
-
-
-
-
-        
-        	
