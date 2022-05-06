44a5d7cfa41ccb1e22079a3cf6946be7361f2d44
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-05-06 15:30:44 +0800

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -10,9 +10,13 @@ from lists.models import Item, List
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response=self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
