b01c5765bc5772aa22043f7180cca0fcec51a0bf
侍嘉玮 <shijiawei@shijiaweideMacBook-Pro.local>
2022-05-07 14:45:51 +0800

    test_form_validation_for_duplicate_items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
+from unittest import skip
 
 
 # Create your tests here.
@@ -114,6 +115,16 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response=self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_pages(self):
+        list1=List.objects.create()
+        item1=Item.objects.create(list=list1, text='textey')
+        response=self.client.post(f'/lists/{list1.id}/', data={'text':'textey'})
+        expected_error=escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTmplatedUsed(response, 'List.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+        
 
     def test_displays_item_form(self):
         list_=List.objects.create()
